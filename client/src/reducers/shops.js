6e15e3aef8ac8b7cd37280e44f30cde194d6dd2e
Marwane Chahoud <marwane.chahoud@gmail.com>
2018-01-29 02:37:02 +0000

    initialize project and adding react-redux to it

---
+++ client/src/reducers/shops.js
@@ -0,0 +1,6 @@
+
+export default function shops(state = [], action = {}) {
+  switch(action.type) {
+  default: return state;
+  }
+}
---
+++ client/src/rootReducer.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+import shops from './reducers/shops';
+
+export default combineReducers({
+  shops
+});
