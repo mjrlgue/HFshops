d8688cdd6b3ab1fca76cf981481b17fd4402b199
Marwane Chahoud <marwane.chahoud@gmail.com>
2018-02-01 03:09:22 +0000

    Login and Logout

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -21,3 +21,11 @@ export function setCurrentUser(user) {
     user
   }
 }
+
+export function logout() {
+  return dispatch => {
+    localStorage.removeItem('jwtToken');
+    setAuthorizationToken(false);
+    dispatch(setCurrentUser({}));
+  }
+}
--- client/src/components/NavigationBar.js
+++ client/src/components/NavigationBar.js
@@ -1,21 +1,66 @@
 import React from 'react';
-
+import PropTypes from 'prop-types';
 import { Link } from 'react-router';
+import { connect } from 'react-redux';
+import { logout } from '../actions/authActions';
 
-export default () => {
 
-  return (
-    <div className="ui secondary menu right floated">
-      <div className="item">
-        <button className="ui small white button ">
-          <Link to="/signup" className="primary">Signup</Link>
-        </button>
+class NavigationBar extends React.Component {
+  logout(e) {
+    e.preventDefault();
+    this.props.logout();
+  }
+
+  render() {
+      const { isAuthenticated } = this.props.auth;
+
+      const userLinks = (
+      <div className="ui secondary menu right floated">
+        <div className="item">
+          <button className="ui small white button ">
+            <Link to="/shops" className="primary">Shops</Link>
+          </button>
+        </div>
+        <div className="item">
+          <button className="ui small white button ">
+            <a hred="#" className="primary" onClick={this.logout.bind(this)}>Logout</a>
+          </button>
+        </div>
       </div>
-      <div className="item">
-        <button className="ui small white button ">
-          <Link to="/login" className="primary">Login</Link>
-        </button>
-      </div>
-    </div>
   );
+
+      const guestLinks = (
+      <div className="ui secondary menu right floated">
+        <div className="item">
+          <button className="ui small white button ">
+            <Link to="/signup" className="primary">Signup</Link>
+          </button>
+        </div>
+        <div className="item">
+          <button className="ui small white button ">
+            <Link to="/login" className="primary">Login</Link>
+          </button>
+        </div>
+      </div>
+      );
+    return (
+      <div>
+        { isAuthenticated ? userLinks : guestLinks }
+      </div>
+    );
+  }
 }
+
+NavigationBar.propTypes = {
+  auth: PropTypes.object.isRequired,
+  logout: PropTypes.func.isRequired
+}
+
+function mapStateToProps(state) {
+  return {
+    auth: state.auth
+  };
+}
+
+
+export default connect(mapStateToProps, { logout })(NavigationBar);
